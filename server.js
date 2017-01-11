778bba9dc613d21ff67969ba758d5371c7ed2c28
Shari Meggs <smeggs1@gmail.com>
2017-01-11 15:42:15 -0800

    Setting up the link for mongo connection

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Assignment = require('./models/assignment');
+var dotenv = require('dotenv');
 var port = process.env.PORT || 8080;
+mongoose.connect(process.env.MONGODB_LINK);
 
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 app.listen(port);
 console.log('Got it up going high when they go low');
