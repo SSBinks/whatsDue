e67ad28fa1411ff2166ff4642e9dd2105ba15e28
Shari Meggs <smeggs1@gmail.com>
2017-01-11 20:02:18 -0800

    Successfully able to post via postman

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var app = express();
+var moment = require('moment');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Assignment = require('./models/assignment');
@@ -16,6 +17,21 @@ var router = express.Router();
 router.use(function(req, res, next) {
   console.log('Its ALIVE');
   next();
+});
+
+router.route('/assign')
+.post( function(req, res){
+  var assign = new Assignment();
+  assign.title = req.body.title;
+  assign.dueDate = moment(req.body.dueDate, "MM-DD-YYYY");
+  assign.progress = req.body.progress;
+  assign.complete = req.body.complete;
+
+  assign.save(function(err){
+    if(err)
+    res.send(err);
+    res.json({ message: "Come on ladies let us get into formation"});
+  });
 });
 app.use('/', router)
 app.listen(port);
