a1880fd86ecdbe518b84c5d712441a78c00cd4a8
Shari Meggs <smeggs1@gmail.com>
2017-01-25 20:34:50 -0800

    Console logs are gone

--- models/assignment.js
+++ models/assignment.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 var moment = require('moment');
 var Schema = mongoose.Schema;
-var AssignmentType = require('./assignmenttype');
 var AssignmentSchema = new Schema ({
   title: String,
   dueDate: {type: Date, max: moment().add(2, 'years').calendar()},
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var moment = require('moment');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Assignment = require('./models/assignment');
-var AssignmentType = require('./models/assignmenttype');
 var dotenv = require('dotenv').config();
 var port = process.env.PORT || 8081;
 mongoose.connect(process.env.MONGODB_LINK);
@@ -16,7 +15,7 @@ app.use(bodyParser.json());
 
 var router = express.Router();
 router.use(function(req, res, next) {
-  console.log('Its ALIVE');
+  console.log('Boot Up');
   next();
 });
 
@@ -24,7 +23,6 @@ router.use(function(req, res, next) {
 router.route('/')
 .get(function(req, res) {
   const today = moment().utc().subtract(1, 'days').format();
-  console.log('Today is ' + today);
   Assignment.find({dueDate: {$gt: today}}).sort({dueDate: 'ascending'}).exec(function (err, assignment){
     if(err)
     res.send(err);
@@ -35,27 +33,20 @@ router.route('/')
 
 router.route('/assign')
 .post( function(req, res){
-  // res.send(err);
   var assign = new Assignment();
-  // console.log('>>>>This is type object' + JSON.stringify(type))
   assign.title = req.body.title;
-  console.log( 'this is Body' + JSON.stringify(req.body));
-  console.log( 'this is Title' + req.body.title);
   assign.dueDate = moment(req.body.dueDate).format('L');
-  console.log( 'this is DATE' + req.body.dueDate);
   assign.progress = req.body.progress;
   assign.complete = req.body.complete;
   assign.categories = req.body.categories;
   assign.part = req.body.part;
-  console.log( 'this is complete' + req.body.completionAmount);
   assign.completionAmount = req.body.completionAmount;
-  console.log( 'this is goal' + req.body.goal);
   assign.goal = req.body.goal;
   assign.dailyGoal = req.body.dailyGoal;
   assign.save(function(err){
     if(err)
     res.send('derp this broke' + err);
-    // console.log('>>>>This is type object ' + JSON.stringify(type))
+
 
   });
 res.json({ message: "Come on ladies let us get into formation"});
@@ -85,14 +76,9 @@ router.route('/assign/update/:assign_id')
   Assignment.findById(req.params.assign_id, function(err, assignment) {
     if(err)
     res.send(err);
-      console.log('the id ' + assignment);
     let keys = Object.keys(req.body);
     for(var i =0; i < keys.length; i++){
-
       let key = keys[i];
-      console.log('the key is ' + key);
-      console.log('the second ' + req.body[key]);
-
       assignment[key] = req.body[key];
     }
     assignment.save(function(err) {
@@ -103,14 +89,7 @@ router.route('/assign/update/:assign_id')
   });
 });
 
-router.route('/assign/categories/:categories')
-.get(function(req, res) {
-AssignmentType.find({categories: req.params.categories}, function(err, type){
-  if(err)
-  res.send(err);
-  res.json(type);
-  });
-})
+
 
 app.use('/', router)
 app.listen(port);
